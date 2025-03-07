f3cb95b1412cd63523e07b324f271f9db4b5cc96
MingLiang-Dong <154511887+MingLiang-Dong@users.noreply.github.com>
2025-03-07 19:58:41 +0100

    v1

---
+++ Frontend_EGM/src/app/entities/direccion/direccion-ajuste/direccion-ajuste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direccion-ajuste',
+  standalone: true,
+  imports: [],
+  templateUrl: './direccion-ajuste.component.html',
+  styleUrl: './direccion-ajuste.component.css'
+})
+export class DireccionAjusteComponent {
+
+}
---
+++ Frontend_EGM/src/app/entities/pago/pago-ajuste/pago-ajuste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-ajuste',
+  standalone: true,
+  imports: [],
+  templateUrl: './pago-ajuste.component.html',
+  styleUrl: './pago-ajuste.component.css'
+})
+export class PagoAjusteComponent {
+
+}
