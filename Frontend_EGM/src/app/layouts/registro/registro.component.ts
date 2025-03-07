2a8d27bcdcf8169dc1cae9e2bec56610e5e8c604
Gertswito <gertjustgonzalezbreto@gmail.com>
2025-03-07 20:35:07 +0100

    Añadidos los iconitos al ser error en el registro también

--- Frontend_EGM/src/app/layouts/registro/registro.component.ts
+++ Frontend_EGM/src/app/layouts/registro/registro.component.ts
@@ -26,7 +26,7 @@ export class RegistroComponent implements OnInit {
 
   ngOnInit(): void {
     this.registroFormulario = new FormGroup({
-      usuario: new FormControl(null, [Validators.required]),
+      usuario: new FormControl(null, [Validators.required, Validators.maxLength(50),]),
       nombre: new FormControl(null, [Validators.required, Validators.maxLength(50),]),
       apellidos: new FormControl(null, [Validators.required, Validators.maxLength(50),]),
       email: new FormControl(null, [Validators.required, Validators.email, Validators.maxLength(100),]),
