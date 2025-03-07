7daadf9bdd7be35b87c91de9fa0fe93a873fda5b
Gertswito <gertjustgonzalezbreto@gmail.com>
2025-03-07 20:29:12 +0100

    Metido limite de 6 caracteres también en el registro

--- Frontend_EGM/src/app/layouts/registro/registro.component.ts
+++ Frontend_EGM/src/app/layouts/registro/registro.component.ts
@@ -34,7 +34,7 @@ export class RegistroComponent implements OnInit {
       tipoDoc: new FormControl(null, [Validators.required]), 
       dni: new FormControl(null, [Validators.required, Validators.minLength(9), Validators.maxLength(9)]),
       fechaNac: new FormControl(null, [Validators.required]),
-      contrasenha: new FormControl(null, [Validators.required]),
+      contrasenha: new FormControl(null, [Validators.required, Validators.minLength(6)]),
       repiteContrasenha: new FormControl(null, [Validators.required]),
     });
   }
