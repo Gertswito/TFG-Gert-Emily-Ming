0b900671482da02cbd8d93047108ba20b55566dd
Gertswito <gertjustgonzalezbreto@gmail.com>
2025-05-29 21:52:36 +0200

    develop Cambiado el estilo de compra-exito

--- Frontend_EGM/src/app/layouts/compra/compra-exito/compra-exito.component.ts
+++ Frontend_EGM/src/app/layouts/compra/compra-exito/compra-exito.component.ts
@@ -2,15 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   standalone: true,
   selector: 'compra',
-  imports: [FormsModule, ReactiveFormsModule, CommonModule, RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './compra-exito.component.html',
   styleUrls: ['./compra-exito.component.css'],
 })
 export class CompraExitoComponent implements OnInit {
+    faCheckCircle = faCheckCircle;
+    
     protected router = inject(Router);
     
     ngOnInit(): void {
